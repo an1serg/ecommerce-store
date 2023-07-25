23f847c4f6533fb4544b83661d7a8018d3d72cfe
an1serg <anissereg@gmail.com>
2023-07-26 02:25:39 +0300

    feat: complete home page, featured products and individual product screen

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,28 @@
+import getBillboard from '@/actions/get-billboard';
+import getProducts from '@/actions/get-products';
+import Billboard from '@/components/billboard';
+import ProductList from '@/components/product-list';
+import Container from '@/components/ui/container';
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+
+  const billboard = await getBillboard(
+    '6412dfec-c193-481e-8b84-7f2c6face185'
+  );
+
+  return (
+    <Container>
+      <div className='space-y-10 pb-10'>
+        <Billboard data={billboard} />
+        <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+          <ProductList title='Featured Products' items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
